2d5e2cdaf544951b67af05489922d0e592a40c0c
Lakshmi Deepak Reddy <100694165+kldr26@users.noreply.github.com>
2022-11-02 14:01:59 +0530

    Add files via upload

---
+++ frontpage.php
@@ -0,0 +1,43 @@
+
+<html lang="en">
+<head>
+<style>
+body {
+ background-image: url("newspic.jpg");
+ background-repeat: no-repeat;
+ background-attachment: fixed;
+ background-size: cover;
+ }
+ button
+ {
+	 background-color:#87CEEB;
+	 width:20%;
+	 border:10px;
+	 padding:14px 28px;
+	 cursor:pointer;
+	 text-align:center;
+	 border: 5px solid black;
+ }
+ h1{
+	 font-size:150px;
+ }
+ h2{
+	 font-size:50px;
+ }
+ </style>
+   
+</head>
+  <body align="center" bgcolor=""> 
+    <table border="1">
+	<br>
+	<h2 style="color:#FFFF00;">Online News Portal </h2>
+	<br>
+    <h1><a href="http://localhost/Team21/userloginwithotp/login-user.php">
+     <button>LOGIN AS USER</button>
+    </a></h1>
+    <h1><a href="http://localhost/Team21/newsportal/admin/">
+    <button>LOGIN AS ADMIN</button><h1>
+    </a>
+    </table>
+  </body>    
+</html>
